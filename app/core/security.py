1ec89da0ee7486a7a1bb99b50ff6d76f30e27282
Sygris <tiago.a.vista@gmail.com>
2026-01-26 20:41:10 +0100

    Starting to implement JWT

--- app/core/security.py
+++ app/core/security.py
@@ -1,11 +1,34 @@
+import os
+from dotenv import load_dotenv
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+
+load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# Password Hashing
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+# JWT
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(user_id: int) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    payload = {"sub": str(user_id), "exp": expire}
+
+    token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
+    return token
